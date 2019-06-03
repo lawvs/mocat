e48872e6339841286c0c31acf7378100f8ea7d72
lawvs <lawvs@users.noreply.github.com>
2019-06-04 01:18:49 +0800

    chore: initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+// https://eslint.org/docs/user-guide/configuring
+// "off" or 0 - turn the rule off
+// "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
+// "error" or 2 - turn the rule on as an error (exit code will be 1)
+
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['prettier'],
+  rules: {
+    // 'semi': ['error', 'never'],
+    // 'quotes': ['error', 'single'],
+    // 'indent': ["error", 2],
+    'linebreak-style': ['error', 'unix'],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-unused-vars': ['warn', { args: 'none' }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-trailing-spaces': 'error',
+    'no-irregular-whitespace': 'error',
+    // 'comma-dangle': ['error', 'always-multiline'],
+    'prettier/prettier': 'warn',
+    // 'react/prop-types': 'off',
+  },
+}
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// https://prettier.io/
+module.exports = {
+  // parser: 'typescript',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  // bracketSpacing: true,
+  // tabWidth: 2,
+  // useTabs: false,
+}
