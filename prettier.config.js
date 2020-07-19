71499c1320da03c755fcc05f5da1fea7d0730581
lawvs <lawvs@users.noreply.github.com>
2020-07-20 02:27:21 +0900

    refactor: update project config

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,5 @@
 // https://prettier.io/
 module.exports = {
-  // parser: 'typescript',
   semi: false,
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
-  // bracketSpacing: true,
-  // tabWidth: 2,
-  // useTabs: false,
+  singleQuote: true
 }
