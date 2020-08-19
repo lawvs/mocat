c5364aa936e0f241e5d756700c2dc1d834f1aaac
lawvs <lawvs@users.noreply.github.com>
2020-08-19 13:19:30 +0800

    chore: upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,15 +12,17 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'plugin:@typescript-eslint/recommended',
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'prettier',
     'prettier/react',
+    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     project: './tsconfig.json',
     ecmaFeatures: {
@@ -29,13 +31,10 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      { multiline: { delimiter: 'none' } },
-    ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'react/prop-types': 'off',
     'linebreak-style': ['error', 'unix'],
     'no-var': 'error',
     'prefer-const': 'error',
@@ -48,4 +47,5 @@ module.exports = {
       version: require('react').version,
     },
   },
+  ignorePatterns: ['*.js'],
 }
