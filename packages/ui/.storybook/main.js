4c0a8ed2adcf52d43eebf4636941744fd985eadf
lawvs <lawvs@users.noreply.github.com>
2020-08-19 15:28:42 +0800

    chore(ui): add storybook addon-controls

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,7 +1,12 @@
 // https://storybook.js.org/docs/react/configure/overview
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-controls',
+  ],
   webpackFinal: (/** @type {import('webpack').Configuration} */ config) => {
     return config
   },
--- packages/ui/src/stories/drawer.stories.tsx
+++ packages/ui/src/stories/drawer.stories.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
+import type { Story } from '@storybook/react/types-6-0'
 import { Drawer } from '../components/drawer'
 
 export default {
   title: 'Drawer',
   component: Drawer,
+  args: {
+    onOpen: action('onOpen'),
+    onClose: action('onClose'),
+    children: 'content',
+  },
+  argTypes: {
+    children: { control: { type: 'text' } },
+  },
 }
 
-export const DrawerComponent = () => (
-  <Drawer onOpen={action('onOpen')} onClose={action('onClose')}>
-    content
-  </Drawer>
+export const Basic: Story<Parameters<typeof Drawer>[0]> = (args) => (
+  <Drawer {...args} />
 )
