4e4d6c545b7604f6626fe0cc711f442be0fc06cf
lawvs <lawvs@users.noreply.github.com>
2020-08-19 13:11:11 +0800

    chore: upgrade storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,19 +1,8 @@
+// https://storybook.js.org/docs/react/configure/overview
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: (/** @type {import('webpack').Configuration} */ config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('ts-loader'),
-        },
-        // {
-        //   loader: require.resolve('react-docgen-typescript-loader'),
-        // },
-      ],
-    })
-    config.resolve.extensions.push('.ts', '.tsx')
     return config
   },
 }
