e7de491cb8e77f84c78500bd0a969e9f511263c1
lawvs <lawvs@users.noreply.github.com>
2020-08-31 01:09:14 +0900

    chore: update base

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'react/prop-types': 'off',
     'linebreak-style': ['error', 'unix'],
     'no-var': 'error',
