362873b12809394c8c46f4f72cec6ec6b875c518
lawvs <lawvs@users.noreply.github.com>
2020-09-08 02:07:18 +0900

    refactor(ui): replace ToggleButton with Floating action button

--- packages/ui/src/components/drawer.tsx
+++ packages/ui/src/components/drawer.tsx
@@ -1,47 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
 import {
   makeStyles,
   useTheme,
   createStyles,
+  Fab,
   Drawer as MUIDrawer,
   Divider,
+  ClickAwayListener,
 } from '@material-ui/core'
 import type { Theme } from '@material-ui/core/styles/createMuiTheme'
 import IconButton from '@material-ui/core/IconButton/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
-
-const ToggleButton = styled.button`
-  position: fixed;
-  right: 0;
-  top: 50%;
-  transform: translate(0, -50%);
-  border-radius: 8px 0 0 8px;
-
-  background-color: #d4d4d4;
-  width: 30px;
-  height: 80px;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  transition: all 225ms;
-  opacity: 0.7;
-  box-shadow: rgba(118, 118, 118, 0.1) -1px 3px 1px 0px;
-
-  &:active {
-    transform-origin: right;
-    transform: translate(0, -50%) scale(1.1);
-  }
-
-  &:hover {
-    opacity: 1;
-    box-shadow: rgba(118, 118, 118, 0.2) -1px 3px 3px 0px;
-  }
-`
+import BuildIcon from '@material-ui/icons/Build'
+import Box from '@material-ui/core/Box/Box'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    fab: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+    fabExtendedIcon: {
+      marginRight: theme.spacing(1),
+    },
     drawerHeader: {
       display: 'flex',
       alignItems: 'center',
@@ -59,26 +42,38 @@ export const Drawer: React.FC = ({ children }) => {
   const classes = useStyles()
   const theme = useTheme()
 
-  const toggleDrawer = () => {
-    setOpen(!open)
-  }
+  const toggleDrawer = () => setOpen(!open)
+  const handleClickAway = () => setOpen(false)
 
   return (
-    <>
-      <ToggleButton onClick={toggleDrawer}></ToggleButton>
-      <MUIDrawer open={open} anchor="right" variant="persistent">
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={toggleDrawer}>
-            {theme.direction !== 'ltr' ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        {children}
-      </MUIDrawer>
-    </>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <Box>
+        {/* Floating action button */}
+        <Fab
+          className={classes.fab}
+          color="primary"
+          variant="extended"
+          aria-label="mock"
+          onClick={toggleDrawer}
+        >
+          <BuildIcon className={classes.fabExtendedIcon} />
+          Mock
+        </Fab>
+
+        <MUIDrawer open={open} anchor="right" variant="persistent">
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={toggleDrawer}>
+              {theme.direction !== 'ltr' ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          {children}
+        </MUIDrawer>
+      </Box>
+    </ClickAwayListener>
   )
 }
