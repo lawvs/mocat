3857ca0bc97991daeb20ae9d3f036f5b520ebfbe
lawvs <lawvs@users.noreply.github.com>
2020-09-08 19:02:30 +0800

    feat(ui): add pin on drawer

--- packages/ui/src/components/drawer.tsx
+++ packages/ui/src/components/drawer.tsx
@@ -3,6 +3,7 @@ import {
   makeStyles,
   useTheme,
   createStyles,
+  Box,
   Fab,
   Drawer as MUIDrawer,
   Divider,
@@ -14,7 +15,8 @@ import IconButton from '@material-ui/core/IconButton/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import BuildIcon from '@material-ui/icons/Build'
-import Box from '@material-ui/core/Box/Box'
+import LockIcon from '@material-ui/icons/Lock'
+import LockOpenIcon from '@material-ui/icons/LockOpen'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -40,12 +42,14 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const Drawer: React.FC = ({ children }) => {
   const [open, setOpen] = React.useState(false)
+  const [pin, setPin] = React.useState(false)
 
   const classes = useStyles()
   const theme = useTheme()
 
-  const toggleDrawer = () => setOpen(!open)
-  const handleClickAway = () => setOpen(false)
+  const toggleDrawer = () => !pin && setOpen(!open)
+  const handleClickAway = () => !pin && setOpen(false)
+  const togglePin = () => setPin(!pin)
 
   return (
     <ClickAwayListener onClickAway={handleClickAway}>
@@ -65,15 +69,20 @@ export const Drawer: React.FC = ({ children }) => {
         </Zoom>
 
         <MUIDrawer open={open} anchor="right" variant="persistent">
+          {/* Header */}
           <div className={classes.drawerHeader}>
-            <IconButton onClick={toggleDrawer}>
+            <IconButton onClick={toggleDrawer} disabled={pin}>
               {theme.direction !== 'ltr' ? (
                 <ChevronLeftIcon />
               ) : (
                 <ChevronRightIcon />
               )}
             </IconButton>
+            <IconButton onClick={togglePin}>
+              {pin ? <LockIcon /> : <LockOpenIcon />}
+            </IconButton>
           </div>
+
           <Divider />
           {children}
         </MUIDrawer>
