f91af1bf9a35a368afc2f9e7c32f8df520c41efb
lawvs <lawvs@users.noreply.github.com>
2020-09-13 22:58:41 +0900

    chore: extract eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,11 @@
-// https://eslint.org/docs/user-guide/configuring
-// "off" or 0 - turn the rule off
-// "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
-// "error" or 2 - turn the rule on as an error (exit code will be 1)
-
+/** @type { import('eslint').Linter.Config } */
 module.exports = {
   root: true,
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/react',
-    'prettier/@typescript-eslint',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'react/prop-types': 'off',
-    'linebreak-style': ['error', 'unix'],
-    'no-var': 'error',
-    'prefer-const': 'error',
-    'no-unused-vars': ['warn', { args: 'none' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'prettier/prettier': 'warn',
-  },
+  extends: ['./packages/eslint-config'],
+  ignorePatterns: ['*.js', 'packages/*/build/**/*.ts'],
   settings: {
     react: {
       version: require('react').version,
     },
   },
-  ignorePatterns: ['*.js', 'packages/*/build/**/*.ts'],
 }
