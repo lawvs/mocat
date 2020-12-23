93fbd8460c9114c4b2b572e120e4669114716577
lawvs <lawvs@users.noreply.github.com>
2020-12-23 19:19:32 +0900

    chore: update action card story

--- packages/ui/src/stories/actionCard.stories.tsx
+++ packages/ui/src/stories/actionCard.stories.tsx
@@ -5,47 +5,84 @@ import { ActionCard } from '../components/actionCard'
 export default {
   title: 'ActionCard',
   component: ActionCard,
-  args: {
-    event: {
-      type: 'Run/network/before',
-      requestType: 'fetch',
-      rule: {
-        name: 'name',
-        desc: 'desc',
-        type: 'Register/networkRoute',
-        url: '/',
-        method: '*',
-        scenarios: [
-          {
-            name: 'scenario 1',
-            desc: 'desc1',
-            status: 200,
-            response: { data: 'success' },
-          },
-          {
-            name: 'scenario 2',
-            desc: 'desc2',
-            status: 400,
-            response: { data: 'error' },
-          },
-          {
-            name: 'scenario 3 without desc',
-            response: { data: 'success' },
-          },
-        ],
-      },
-
-      request: new Request(''),
-      resolve: action('resolve'),
-      reject: action('reject'),
-      pass: action('pass'),
-    },
-  },
-  argTypes: {
-    // children: { control: { type: 'text' } },
-  },
+  argTypes: {},
 }
 
 export const Basic: Story<Parameters<typeof ActionCard>[0]> = (args) => (
   <ActionCard {...args}></ActionCard>
 )
+
+Basic.args = {
+  event: {
+    type: 'Run/network/before',
+    requestType: 'fetch',
+    timeStamp: 1,
+    rule: {
+      name: 'name',
+      desc: 'desc',
+      type: 'Register/networkRoute',
+      url: '/',
+      timeStamp: 1,
+      scenarios: [
+        {
+          name: 'scenario 1',
+          desc: 'desc1',
+          status: 200,
+          response: { data: 'success' },
+        },
+        {
+          name: 'scenario 2',
+          desc: 'desc2',
+          status: 400,
+          response: { data: 'error' },
+        },
+        {
+          name: 'scenario 3 without desc',
+          response: { data: 'success' },
+        },
+      ],
+    },
+
+    request: new Request(''),
+    resolve: action('resolve'),
+    reject: action('reject'),
+    pass: action('pass'),
+  },
+}
+
+export const PostCard: Story<Parameters<typeof ActionCard>[0]> = (args) => (
+  <ActionCard {...args}></ActionCard>
+)
+
+PostCard.args = {
+  event: {
+    type: 'Run/network/before',
+    requestType: 'xhr',
+    timeStamp: 1,
+    rule: {
+      name: 'name',
+      desc: 'desc',
+      type: 'Register/networkRoute',
+      url: '/',
+      timeStamp: 1,
+      scenarios: [
+        {
+          name: 'scenario 1',
+          response: { data: 'success' },
+        },
+      ],
+    },
+
+    request: new Request('', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer xxxxx',
+      },
+      body: JSON.stringify({ a: 1 }),
+    }),
+    resolve: action('resolve'),
+    reject: action('reject'),
+    pass: action('pass'),
+  },
+}
