d24263a08b5cdcd5b7d42000d95d669d758268fd
Whitewater <litusq@gmail.com>
2022-06-12 18:28:56 +0000

    fix: e2e

--- examples/spa/vite.config.js
+++ examples/spa/vite.config.js
@@ -4,6 +4,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    port: 3003
+  },
   plugins: [
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
