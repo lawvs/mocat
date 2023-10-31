d536abddd3002e493d0d940bc6dc2f08f849e507
lawvs <18554747+lawvs@users.noreply.github.com>
2023-10-31 22:53:01 +0800

    chore: upgrade eslint

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -15,6 +15,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
